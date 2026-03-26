b8194b5eb8cc1c48945ab45ec251a4c64514e395
Atrex10 <wojciech.sowa@onet.com.pl>
2026-03-26 20:41:25 +0100

    Add AOD clock nums bitmaps

--- src/clockNum.h
+++ src/clockNum.h
@@ -6,4 +6,10 @@ extern const int clockNumHeights[10];
 
 extern const unsigned char clockNumBmps[10][144];
 
+// AOD clock nums
+extern const int AODClockNumWidhts[10];
+extern const int AODClockNumHeights[10];
+
+extern const unsigned char AODClockNumBmps[10][144];
+
 #endif
